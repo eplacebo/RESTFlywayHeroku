70d228295e0ede66c266d6b7029ab1ff549d3509
eplacebo <eplacebo@yandex.ru>
2021-10-09 15:13:09 +0700

    heroku bd

--- src/main/java/rest/StartServlet.java
+++ src/main/java/rest/StartServlet.java
@@ -1,6 +1,7 @@
 package rest;
 
 import entity.User;
+import org.flywaydb.core.Flyway;
 import service.NoteService;
 import service.impl.NoteServiceImpl;
 
@@ -17,6 +18,9 @@ public class StartServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
+        Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://ec2-52-30-81-192.eu-west-1.compute.amazonaws.com:5432/d7us9cuv0gfcfh", "ykdnhbaritwmgn", "9f9e57371797d72553144bb143670029842475cb1dca453aefbf1e2f28761f34").load();
+        flyway.repair();
+        flyway.migrate();
         noteService = new NoteServiceImpl();
     }
 
